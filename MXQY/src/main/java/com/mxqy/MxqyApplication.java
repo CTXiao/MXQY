d33cf0115c60dbb89f23147e394009d211e9fcdb
CTXiao <294295992@qq.com>
2018-03-04 20:56:48 +0800

    MXQY

---
+++ MXQY/src/main/java/com/mxqy/MxqyApplication.java
@@ -0,0 +1,12 @@
+package com.mxqy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MxqyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MxqyApplication.class, args);
+	}
+}
